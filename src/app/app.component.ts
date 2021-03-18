75176d5dc8794446bd9009c3b393ccc82e71cfe8
wilhelmfrank <frank@mpdl.mpg.de>
2021-03-18 13:57:36 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'r2d2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'r2d2-ng';
+}
