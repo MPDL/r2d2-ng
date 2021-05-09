eb605bf2b82b8e4b219f3eea13bbdefb075b67d9
wilhelmfrank <frank@mpdl.mpg.de>
2021-05-09 17:08:12 +0200

    adopt edmond style ...

--- src/app/core/components/foot/foot.component.ts
+++ src/app/core/components/foot/foot.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'r2d2-foot',
   templateUrl: './foot.component.html'
 })
 export class FootComponent implements OnInit {
+
+  version = environment.ver;
 
   constructor() {}
 
