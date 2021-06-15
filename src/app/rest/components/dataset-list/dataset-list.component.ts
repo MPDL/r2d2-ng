5f2dd09ed9393336bb666b8a2b477a51a8d09818
wilhelmfrank <frank@mpdl.mpg.de>
2021-06-15 16:11:11 +0200

    ready 4 12

--- src/app/rest/components/dataset-list/dataset-list.component.ts
+++ src/app/rest/components/dataset-list/dataset-list.component.ts
@@ -1,16 +1,12 @@
+
 import { Component, OnInit } from '@angular/core';
 import { R2d2Service } from '../../services/r2d2.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-import {
-  ESTO,
-  DatasetVersion,
-} from '../../../shared/components/model/entities';
+import { ESTO, DatasetVersion } from '../../../shared/components/model/entities';
 import { AuthenticationService } from '../../../core/services/authentication.service';
 import { map } from 'rxjs/operators';
 import { AggregationService } from '../../services/aggregation.service';
-import { PaginationDirective } from '../../directives/pagination.directive';
-import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'r2d2-dataset-list',
@@ -18,76 +14,59 @@ import { environment } from '../../../../environments/environment';
   templateUrl: './dataset-list.component.html'
 })
 export class DatasetListComponent implements OnInit {
+
   datasets: Observable<DatasetVersion[]>;
   p: number = 1;
   users: Observable<any>;
   no_name = 'n/a';
   search_term: string;
-  // Facets
-  listUrl: string;
   affiliations_obs: Observable<{}[]>;
   genres_obs: Observable<{}[]>;
-  // Pagination:
-  totalItems: number;
-  pageSize: number = 10;
-  numPages: number;
-  maxRange: number = 1;
-  currentPage: number = 1;
 
   constructor(
     private service: R2d2Service,
     private router: Router,
     public auth: AuthenticationService,
     private aggs: AggregationService,
-    public pagination: PaginationDirective
-  ) {}
+
+  ) { }
 
   ngOnInit(): void {
-    this.listUrl = environment.r2d2_rest_uri;
     this.datasets = this.service.list().pipe(
       // map(result => result.total > 0 ? result.hits.map(ito => ito.source) : [])
-      map((result) => {
-        if (result.total > 0) {
-          this.totalItems = result.total;
-          this.numPages = Math.ceil(this.totalItems/this.pageSize);
-        }; 
-        return result.hits?.map((ito) => ito.source);
-      })
+      map(result => result.hits?.map(ito => ito.source))
     );
     const affiliations = {
       terms: {
         field: 'metadata.authors.affiliations.organization.keyword',
         order: { _count: 'desc' },
-        size: 25,
-      },
+        size: 25
+      }
     };
     const genres = {
       terms: {
         field: 'metadata.genres.keyword',
         order: { _count: 'desc' },
-        size: 25,
-      },
+        size: 25
+      }
     };
     const nested = {
       nested: {
-        path: 'metadata.authors',
+        path: 'metadata.authors'
       },
-      aggregations: { affiliations },
+      aggregations: { affiliations }
     };
     // nested returns accumulated number of all affiliations ...
     // since nested authors are stored in seperate documents!
     this.affiliations_obs = this.aggs.getBuckets('', { affiliations });
     this.genres_obs = this.aggs.getBuckets('', { genres });
+
   }
 
   filter(): void {
-    this.datasets = this.service
-      .list(this.search_term)
-      .pipe(
-        map((result) =>
-          result.total > 0 ? result.hits.map((ito) => ito.source) : []
-        )
-      );
+    this.datasets = this.service.list(this.search_term).pipe(
+      map(result => result.total > 0 ? result.hits.map(ito => ito.source) : [])
+    );
     this.search_term = undefined;
   }
 
@@ -128,33 +107,9 @@ export class DatasetListComponent implements OnInit {
     }
 
     this.datasets = this.aggs.termfilter(field, value).pipe(
-      map((response) => {
-        return (response as any).hits.hits.map((hit) => hit._source);
+      map(response => {
+        return (response as any).hits.hits.map(hit => hit._source);
       })
     );
   }
-
-  selectors(counter: number): Array<number> {
-    return Array(counter);
-  }
-
-  onPageChange(pageNo: number) {
-    this.currentPage = pageNo;
-    let from = pageNo * this.pageSize - this.pageSize;
-    this.datasets = this.service
-      .page(from.toString(), this.pageSize.toString())
-      .pipe(
-        map((result) =>
-          result.total > 0 ? result.hits.map((ito) => ito.source) : []
-        )
-      );
-  }
-
-  pageSizeHandler(event: any) {
-    let relocate =  this.currentPage*this.pageSize;
-    this.pageSize = Number.parseInt(event.target.value);
-    this.numPages = Math.ceil(this.totalItems/this.pageSize);
-    this.currentPage = Math.floor(relocate/this.pageSize);
-  }
-
 }
